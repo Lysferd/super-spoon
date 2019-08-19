a2f44dbb61ab982d6e2e9887195b3fe3e8d3e2f6
Lysferd <phabio_almeida@hotmail.com>
2019-08-19 20:55:12 -0300

    Finishing Touches on Index
    Still considered an alpha, but now supposedly working as intended.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,17 +4,21 @@ class HomeController < ApplicationController
 
   def index
 
-    @facilities = Facility.all
-
     if params[:purpose] == 'particular'
       @visitors = Visitor.all
     else
       @visitors = Employee.all
     end
 
-    if !@facility
-      @facility = Facility.find_by(id: params[:facility].to_i)
+    if params[:facility]
+      params[:facility_id] = params[:facility][:id]
     end
+
+    if params[:facility_id] #!@facility
+      @facility = Facility.find_by(id: params[:facility_id])
+    end
+
+    @valid_cpf = CPF.verify params[:cpf] if params[:cpf]
 
     if !@visitor #and @valid_cpf
       if params[:purpose] == 'particular'
@@ -27,8 +31,8 @@ class HomeController < ApplicationController
         if params[:purpose] == 'particular'
           @visitor = Visitor.new(cpf: params[:cpf], name: params[:name])
         else
-          @visitor = Employee.new(cpf: params[:cpf], name: params[:name],
-                                  company: params[:company])
+          params[:company_id] = params[:company][:id] if params[:company]
+          @visitor = Employee.new(cpf: params[:cpf], name: params[:name], company_id: params[:company_id])
         end
       end
     end
@@ -53,9 +57,7 @@ class HomeController < ApplicationController
       end
 
       if @appointment.save
-        redirect_to(@appointment)
-      else
-        redirect_to root_path
+        redirect_to root_path, notice: 'Visita agendada com sucesso!'
       end
     end
   end
